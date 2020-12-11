b27cda57db1794760110b7ef7118af95fe0296cd
Shakiba-e-nur <shakinur1995@gmail.com>
2020-12-12 01:07:18 +0600

    init commit

---
+++ src/main/java/com/faisal/canteenapp/canteenapp/CanteenappApplication.java
@@ -0,0 +1,13 @@
+package com.faisal.canteenapp.canteenapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanteenappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanteenappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/faisal/canteenapp/canteenapp/CanteenappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faisal.canteenapp.canteenapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanteenappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
